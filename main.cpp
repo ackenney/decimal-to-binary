7028c43bf7cd6e8668c579c7d78fc1d612cac58a
ackenney <ackenney@gmail.com>
2024-02-11 17:15:23 -0400

    add contant values

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 // This c++ program converts decimal numbers to binary, hexadecimal, and Binary Coded Decimal.
 
 #include<iostream>
+#include <stack>
+#include <fstream>
+
+// Constant base values
+const int HEXADECIMAL_BASE = 16;
+const int BINARY_BASE = 2;
+const int BINARY_BIT_LENGTH = 4;
+const int HEX_BIT_LENGTH = 2;
+const int BCD_DIGIT_LENGTH = 3;
+const int INPUT_SET_LENGTH = 255;
+
 
 int main()
 {
